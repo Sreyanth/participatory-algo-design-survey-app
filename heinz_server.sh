5ff0d6089997325f6beda14eb75eb84ca001b478
Sreyantha Chary <Sreyanth@users.noreply.github.com>
2021-04-09 00:40:28 -0400

    Update heinz server script to include gunicorn start and restart commands

--- heinz_server.sh
+++ heinz_server.sh
@@ -1,1 +1,8 @@
-sudo ../venv/bin/python3 manage.py runserver 0.0.0.0:80
+# Regular manual server
+# sudo ../venv/bin/python3 manage.py runserver 0.0.0.0:80
+
+# Run the server using gunicorn
+sudo ../venv/bin/gunicorn -b 0.0.0.0:80 --env DJANGO_SETTINGS_MODULE=survey.settings -D survey.wsgi
+
+# Restart the server gracefully
+# kill -HUP `ps -C gunicorn fch -o pid | head -n 1`
