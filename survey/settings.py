b0020d3af085876c03a1a7b7ed67c499908d1e66
Sreyantha Chary <Sreyanth@users.noreply.github.com>
2021-05-09 20:50:34 -0400

    Update settings.py to use Postgres

--- survey/settings.py
+++ survey/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'survey.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': 'survey',
+            'USER': 'survey_user',
+            'PASSWORD': 'Survey@123',
+            'HOST': 'localhost',
+            'PORT': '',
+        }
+    }
 
 
 # Password validation
