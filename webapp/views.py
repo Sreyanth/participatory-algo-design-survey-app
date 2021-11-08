3a4a6d47d85b6cc96832873d0cb5ff581ea47c7c
Sreyanth <sreyanth@gmail.com>
2021-11-08 17:46:22 -0500

    implement an additional mturk ID check to prevent users from taking the survey multiple times

--- webapp/views.py
+++ webapp/views.py
@@ -16,6 +16,8 @@ from .models import (MechTaskAlgorithm, MechTaskStudentSample,
                      MechTaskSurveyEstimate, MechTaskSurveyResponse,
                      MechTaskUserGroup)
 
+PREVIOUSLY_TAKEN_THE_SURVEY = []
+
 STUDENT_ATTRIBUTES = OrderedDict()
 STUDENT_ATTRIBUTES['Student Characteristics'] = [
     {
@@ -273,6 +275,10 @@ class ConsentView(View):
             error_message = 'You need to reconfirm your MTurk ID to proceed'
         elif mturk_id_1 != mturk_id_2:
             error_message = "The MTurk IDs you provided don't match. Try again"
+        elif mturk_id_1 in PREVIOUSLY_TAKEN_THE_SURVEY:
+            error_message = "You've previously taken this survey. Thanks for your interest, but you can't participate in this survey again."
+        elif mturk_id_1 in MechTaskSurveyResponse.objects.values_list('final_mturk_id', flat=True):
+            error_message = "You've already taken this survey. Thanks for your interest, but you can't participate in this survey again."
 
         if error_message:
             page_params = {
